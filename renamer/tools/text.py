55f9168310e8bcebb231f531d9a4ab1aaa8df1df
shiva20991 <80451254+shiva20991@users.noreply.github.com>
2021-06-09 07:39:58 +0530

    Update text.py

--- renamer/tools/text.py
+++ renamer/tools/text.py
@@ -1,13 +1,13 @@
 from pyrogram.emoji import *
 
 class TEXT:
-    DOWNLOAD_START = f"Downloading Starts soon {SLEEPING_FACE}"
-    UPLOAD_START = f"Upload Starting Soon {SLEEPING_FACE}"
-    UPLOAD_SUCESS = f"Thanks for using [me](https://github.com/Ns-AnoNymouS/TG-RENAMER-BOT)"
+    DOWNLOAD_START = f"⚡️𝕯𝖔𝖜𝖓𝖑𝖔𝖆𝖉𝖎𝖓𝖌 𝕾𝖙𝖆𝖗𝖙𝖘 𝖘𝖔𝖔𝖓⚡️ {SLEEPING_FACE}"
+    UPLOAD_START = f"🔴𝖀𝖕𝖑𝖔𝖆𝖉 𝕾𝖙𝖆𝖗𝖙𝖎𝖓𝖌 𝕾𝖔𝖔𝖓..🟤 {SLEEPING_FACE}"
+    UPLOAD_SUCESS = f"𝕿𝖍𝖆𝖓𝖐𝖘 𝖋𝖔𝖗 𝖚𝖘𝖎𝖓𝖌🖨️🔒⚔️ [me](https://t.me/tamilrockersvpn)"
     BANNED_USER_TEXT = f"Hey bro, you are **banned** from using me {FACE_WITH_TEARS_OF_JOY}."
     NOT_LOGGED_TEXT = f"This bot was only for private use {LOCKED_WITH_KEY}. If you want to use this bot you need to send me correct password in the format `/login bot_password`"
-    SAVED_CUSTOM_THUMBNAIL = f"Thumbnail Saved Permanently {NOTEBOOK_WITH_DECORATIVE_COVER}"
-    DELETED_CUSTOM_THUMBNAIL = f"Thumbnail Deleted Successfully {CHECK_MARK_BUTTON}"
+    SAVED_CUSTOM_THUMBNAIL = f"⭕️𝓣𝓱𝓾𝓶𝓫𝓷𝓪𝓲𝓵 𝓢𝓪𝓿𝓮𝓭 𝓟𝓮𝓻𝓶𝓪𝓷𝓮𝓷𝓽𝓵𝔂 {NOTEBOOK_WITH_DECORATIVE_COVER}"
+    DELETED_CUSTOM_THUMBNAIL = f"⋆⋆𝒯𝒽𝓊𝓂𝒷𝓃𝒶𝒾𝓁 𝒟𝑒𝓁𝑒𝓉𝑒𝒹 𝒮𝓊𝒸𝒸𝑒𝓈𝓈𝒻𝓊𝓁𝓁𝓎🎀 {CHECK_MARK_BUTTON}"
     NO_CUSTOM_THUMB_NAIL_FOUND = f"𝖭𝗈 𝗍𝗁𝗎𝗆𝖻𝗇𝖺𝗂𝗅 𝖥𝗈𝗎𝗇𝖽 {THUMBS_DOWN_LIGHT_SKIN_TONE}"
     THUMBNAIL_CAPTION = f"{BACKHAND_INDEX_POINTING_UP_LIGHT_SKIN_TONE} Your Permanent thumbnail"
 
@@ -16,27 +16,17 @@ class TEXT:
 
 ** My Name:** {bot_name}
     
-** Language:** [Python 3](https://www.python.org/)
-
-** FrameWork:** [Pyrogram](https://github.com/pyrogram/pyrogram)
-
-** Developer:** {bot_owner}
-
-** Channel:** [NS BOT UPDATES](https://t.me/Ns_bot_updates)
-
-** Group:** [Ns BOT SUPPORT](https://t.me/Ns_Bot_supporters)
-
-** Source Code:** [Press Me](https://github.com/Ns-AnoNymouS/TG-RENAMER-BOT)
+** 𝕾𝖚𝖇𝖘𝖈𝖗𝖎𝖇𝖊 𝕿𝖔 𝕺𝖚𝖗 𝖈𝖍𝖆𝖓𝖓𝖊𝖑 @TAMILROCKERSVPN
 """
 
     HELP_USER = """**Follow Below Steps:**
    
-☞︎︎︎ Use /mode command to change upload mode.
-☞︎︎︎ Send a photo to set as permanent thumbnail.
-☞︎︎︎ Now send me the Telegram file you want to rename.
-☞︎︎︎ Send the new name when bot ask.
+𝔘𝔰𝔢 /𝔪𝔬𝔡𝔢 𝔠𝔬𝔪𝔪𝔞𝔫𝔡 𝔱𝔬 𝔠𝔥𝔞𝔫𝔤𝔢 𝔲𝔭𝔩𝔬𝔞𝔡 𝔪𝔬𝔡𝔢.
+☞︎︎︎ 𝔖𝔢𝔫𝔡 𝔞 𝔭𝔥𝔬𝔱𝔬 𝔱𝔬 𝔰𝔢𝔱 𝔞𝔰 𝔭𝔢𝔯𝔪𝔞𝔫𝔢𝔫𝔱 𝔱𝔥𝔲𝔪𝔟𝔫𝔞𝔦𝔩.
+☞︎︎︎ 𝔑𝔬𝔴 𝔰𝔢𝔫𝔡 𝔪𝔢 𝔱𝔥𝔢 𝔗𝔢𝔩𝔢𝔤𝔯𝔞𝔪 𝔣𝔦𝔩𝔢 𝔶𝔬𝔲 𝔴𝔞𝔫𝔱 𝔱𝔬 𝔯𝔢𝔫𝔞𝔪𝔢.
+☞︎︎︎ 𝔖𝔢𝔫𝔡 𝔱𝔥𝔢 𝔫𝔢𝔴 𝔫𝔞𝔪𝔢 𝔴𝔥𝔢𝔫 𝔟𝔬𝔱 𝔞𝔰𝔨.
 
-For source code check about
+For DETAILS
 """
 
     START_TEXT = """Hi {user_mention},
@@ -59,4 +49,4 @@ __--Payment Methods:--__
  
 GooglePay / Paytm / PhonPay / paypal / Net Banking
  
-**For Donate:** message me @MaxxRiderz"""
+**For Donate:** message me @TRVPNADMIN"""
